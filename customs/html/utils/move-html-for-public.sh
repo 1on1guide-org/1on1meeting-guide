817f359c3c77020a9b928b0067ae24f5c2ce6877
eroccowaruico <naohito@analog.bz>
2024-05-04 14:15:25 +0900

    デプロイ用のHTML生成の方法を移動に変更

---
+++ customs/html/utils/move-html-for-public.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+TOOL_DIR=$(cd $(dirname $0); pwd)
+HTML_TOOL_PATH=${TOOL_DIR%%\/utils}
+INDEX_DIR=${TOOL_DIR%%\/customs\/html\/utils}
+
+echo ${TOOL_DIR}
+echo ${HTML_TOOL_PATH}
+echo ${INDEX_DIR}
+
+cd ${INDEX_DIR}
+
+SOURCE_DIR=/public/html
+DESTNATION_DIR=/public
+
+mv ${SOURCE_DIR}/* ${DESTNATION_DIR}
+
