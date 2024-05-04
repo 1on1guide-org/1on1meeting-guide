278f3957e39e38f49f5d4c835a31a80905efed9c
eroccowaruico <naohito@analog.bz>
2024-05-04 14:18:26 +0900

    デプロイ用のHTML生成の方法を移動に変更に伴い、public配下にhtmlをコピーしない

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -72,5 +72,3 @@ HTML_PARTS_PATH=${CURRENT_PATH}/customs/html
 HTML_PARAMETERS=" -D ${HTML_OUTPUT_PATH}/ -a imagesdir=images/ -a imagesoutdir=${HTML_OUTPUT_PATH}/images/ -a docinfodir=${HTML_PARTS_PATH}/docinfo -a nofooter "
 # convert
 asciidoctor ${COMMON_PARAMETERS} ${HTML_PARAMETERS} -a convert-for-readers 'index*.adoc'
-# サイト表示用の階層のためにコピー
-cp -rf ./public/html/index.html ./public/index.html
