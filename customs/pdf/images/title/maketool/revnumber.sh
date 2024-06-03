f388868b5fda7a870668857cad38f1e60d80c957
eroccowaruico <naohito@analog.bz>
2024-06-03 19:29:30 +0900

    削除した機能を回復

--- customs/pdf/images/title/maketool/revnumber.sh
+++ customs/pdf/images/title/maketool/revnumber.sh
@@ -9,6 +9,6 @@ echo ${TOOL_DIR}
 echo ${TITLE_DIR}
 echo ${INDEX_DIR}
 
-REVISION_NUMBER=`cat ${INDEX_DIR}//modules/ROOT/partials/defines/define-version-headeronly.adoc | grep revnumber | awk '{print $2}'`
+REVISION_NUMBER=`cat ${INDEX_DIR}/guide/modules/ROOT/partials/defines/define-version-headeronly.adoc | grep revnumber | awk '{print $2}'`
 TITLE_BACKGROUND=`cat ${TITLE_DIR}/title-background.svg`
 echo ${TITLE_BACKGROUND} | sed -e "s|{%revnumber%}|$REVISION_NUMBER|g" > "${TITLE_DIR}/title-background.svg"
